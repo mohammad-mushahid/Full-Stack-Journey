dbcd8c7ec2d7f74dd469a5aa355be77515730b28
Mohammad Mushahid <mohammadmushahid217@gmail.com>
2026-05-01 00:01:34 +0530

    DAY01 _ADD TWO NUMBERS

---
+++ DSA/DAY01 _ADD  TWO NUMBERS.py
@@ -0,0 +1,8 @@
+#TAKING THE FIRST NUMBER
+a=int(input("Enter the first number :"))
+#TAKING THE SECOND NUMBER
+b=int(input("Enter the second number :"))
+#ADDING THE BOTH NUMBER TOGETHER
+total=a+b
+#PRINTING THE RESULT
+print("Sum is:", total)
